55e7e04c8aa447d5511e8004ad28ed5f61c1df36
rudra-rnt <rudranarayantiwari@outlook.com>
2024-07-16 00:46:13 +0530

    2nd program of Assignment 4. Ammendment of documentation

--- Assignments/Assignment4/current_previous_number.py
+++ Assignments/Assignment4/current_previous_number.py
@@ -1,9 +1,15 @@
 # Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number
 
 def print_current_previous_num(cnum,pnum):
-    # cnum = 0
-    # pnum = 0
+    """
+        Write a program to iterate first 10 numbers.
+        Each iteration should print sum of current and previous number.
 
+        Parameters:
+        cnum (int): Initialize current number
+        pnum (int): Initialize second number
+
+    """
     print(f"Printing current and previous number sum in the range (10)")
     for i in range(10):
         sum = cnum + pnum
