a840f74169695acac176bc7bd3375ddd5f111e7e
mkdubey21 <mkdubey21@gmail.com>
2024-07-19 19:53:24 +0530

    Old Files added here

--- JumpStart/week_4/w4_assignment/w4_assignment_2.py
+++ JumpStart/week_4/w4_assignment/w4_assignment_2.py
@@ -4,10 +4,14 @@
 print("=======================_Assignment-2_==========================")
 
 sum = 0
-for i in range (1, 10+1):
+for i in range (10):
     # print(i)
-    priev_num = i-1
-    curr_num = i
+    if i == 0:
+        priev_num = 0
+        curr_num = 0
+    else:
+        priev_num = i-1
+        curr_num = i
+
     sum = priev_num + curr_num
-    sum = priev_num + curr_num
-    print(f"Sr.No._: {i}. current Number is {curr_num}, previous Number is {priev_num}, and sum: ", sum)
+    print(f"Sr.No._: {i+1}. current Number is {curr_num}, previous Number is {priev_num}, and sum: ", sum)
