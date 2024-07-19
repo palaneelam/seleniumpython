6ddf7b2c4a47cc808e25cec69bd805c44c181bf0
mkdubey21 <mkdubey21@gmail.com>
2024-07-19 20:09:28 +0530

    Code Review Changes

--- JumpStart/week_4/w4_assignment/w4_assignment_1.py
+++ JumpStart/week_4/w4_assignment/w4_assignment_1.py
@@ -3,15 +3,49 @@
 
 print("=======================_Assignment-1_==========================")
 
-num1 = 100
-num2 = 11
+def prodSum(num1, num2):
+    prod = num1 * num2
+    sum = num1 + num2
+    result = 0
+    if prod <= 1000:
+        result = prod
+    else:
+        result = sum
 
-prod = num1 * num2
-sum = num1 + num2
+    # print("Result is: ", result)
+    return result
 
-print("First Num: ", num1)
-print("Second Num: ", num2)
-if prod <= 1000:
-    print("prod: ", prod)
-else:
-    print("sum: ", sum)
+
+num1 = int(input("Please Enter First Number: "))
+num2 = int(input("Please Enter Second Number: "))
+
+print("Final Result is: ", prodSum(num1, num2))
+
+
+# ----------------------Learning--------------------------------------
+# num1 = 100
+# num2 = 11
+#
+# prod = num1 * num2
+# sum = num1 + num2
+#
+# print("First Num: ", num1)
+# print("Second Num: ", num2)
+# if prod <= 1000:
+#     print("prod: ", prod)
+# else:
+#     print("sum: ", sum)
+
+
+# num1 = int(input("Please Enter First Number: "))
+# num2 = int(input("Please Enter Second Number: "))
+#
+# prod = num1 * num2
+# sum = num1 + num2
+#
+# print("First Num: ", num1)
+# print("Second Num: ", num2)
+# if prod <= 1000:
+#     print("prod: ", prod)
+# else:
+#     print("sum: ", sum)
