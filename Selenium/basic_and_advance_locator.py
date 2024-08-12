8c1d0c66198e638c5b1d38853911dbf8e2ca80b9
mkdubey21 <mkdubey21@gmail.com>
2024-08-12 07:17:21 +0530

    Locator - Practice by 12-Aug

--- Selenium/basic_and_advance_locator.py
+++ Selenium/basic_and_advance_locator.py
@@ -38,7 +38,11 @@ print("Curr URL: ", driver.current_url)
 # print("By.TAG_NAME: ", driver.find_element(By.TAG_NAME, "h1").text)
 
 # # driver.find_element(By.CLASS_NAME
-# # print("By.CLASS_NAME (Full NAME): ", driver.find_element(By.CLASS_NAME, "title-post entry-title").text)
+# print("By.CLASS_NAME (Full NAME): ", driver.find_element(By.CLASS_NAME, "title-postentry-title").text)
+## - TBD with Neelam
+# CLASS_NAME = driver.find_element(By.CLASS_NAME, ".title-post entry-title")
+# CLASS_NAME = driver.find_element(By.CLASS_NAME, ".title-post.entry-title")
+# print("CLASS_NAME: ", CLASS_NAME.text)
 # print("By.CLASS_NAME (Partial NAME-FIRST): ", driver.find_element(By.CLASS_NAME, "title-post").text)
 # print("By.CLASS_NAME (Partial NAME-LAST): ", driver.find_element(By.CLASS_NAME, "entry-title").text)
 
@@ -131,15 +135,40 @@ print("Curr URL: ", driver.current_url)
 # Relative xpath
 # // : select node from anywhere in html
 
-driver.find_element(By.XPATH, "//input[@id='name']").send_keys("By.XPATH, //input[@id='name']")
-time.sleep(3)
-driver.find_element(By.XPATH, "//input[@name='name']").send_keys("By.XPATH, //input[@name='name']")
+# driver.find_element(By.XPATH, "//input[@id='name']").send_keys("By.XPATH, //input[@id='name']")
+# time.sleep(3)
+# driver.find_element(By.XPATH, "//input[@name='name']").send_keys("By.XPATH, //input[@name='name']")
+#
+# driver.find_element(By.XPATH, "(//input)[5]").send_keys("INPUT - NAME : By.XPATH, //input)[5]")
+# driver.find_element(By.XPATH, "(//input)[6]").send_keys("INPUT - EMAIL : By.XPATH, //input)[6]")
+#
+# # Combination
+# driver.find_element(By.XPATH, "//input[@type='text' and @id='name']").send_keys("INPUT - NAME : By.XPATH >> Combination")
+# driver.find_element(By.XPATH, "//input[@type='text' and @name='email']").send_keys("INPUT - EMAIL : By.XPATH >> Combination")
+#
+label_1 = driver.find_element(By.XPATH, "//label[text()='Name:']").text
+print("By.XPATH - Tag and text - Label_1: ", label_1)
 
-driver.find_element(By.XPATH, "(//input)[5]").send_keys("INPUT - NAME : By.XPATH, //input)[5]")
-driver.find_element(By.XPATH, "(//input)[6]").send_keys("INPUT - EMAIL : By.XPATH, //input)[6]")
+label_2_DD = driver.find_element(By.XPATH, "//label[text()='Dropdown Example:']").text
+print("By.XPATH - Tag and text - label_2_DD: ", label_2_DD)
 
-# Combination
-driver.find_element(By.XPATH, "//input[@type='text' and @id='name']").send_keys("INPUT - NAME : By.XPATH >> Combination")
-driver.find_element(By.XPATH, "//input[@type='text' and @name='email']").send_keys("INPUT - EMAIL : By.XPATH >> Combination")
+print("By.XPATH - Contains: ", driver.find_element(By.XPATH, "//label[contains(text(), 'Checkbox example:')]"))
+
+print("By.XPATH - Contains: ", driver.find_element(By.XPATH, "//label[contains(text(),'Reset Dummy Button:')]"))
+print("By.XPATH - Contains: ", driver.find_element(By.XPATH, "//label[contains(text(),'Dummy')]"))
+print("By.XPATH - Contains: ", driver.find_element(By.XPATH, "//label[contains(text(),'Button')]"))
+print("By.XPATH - Contains: ", driver.find_element(By.XPATH, "//label[contains(text(),'Button')]"))
+
+driver.find_element(By.XPATH, "//label[starts-with(text(),'Radio')][1]")
+driver.find_element(By.XPATH, "//label[starts-with(text(),'Switch')][2]")
+
+print("Label:", driver.find_element(By.XPATH, "//input[starts-with(@id,'chbValue1')]").text)
+print("Label:", driver.find_element(By.XPATH, "//input[starts-with(@id,'chbValue1')]").text)
+
+print("Print Label: ", driver.find_element(By.XPATH, "//a[text()='Sample Link']/parent::label"))
+
+driver.find_element(By.XPATH, "//select[@id='listbox']/child::option[2]")
+driver.find_element(By.XPATH, "//label[@for='sampleLink']/a[1]").click()
+
 print("=============Done===============")
 
